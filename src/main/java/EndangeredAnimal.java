6b44b82831f8c57fb51e99768d4f15f46d5d27c2
liciolentimo <lentimo@gmail.com>
2019-02-15 12:21:28 +0300

    [Finishes #164000986] Implemented interface methods in endangered animal

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.ArrayList;
 import org.sql2o.*;
 
 public class EndangeredAnimal extends Animal implements AnimalInterface{
@@ -31,6 +32,41 @@ public class EndangeredAnimal extends Animal implements AnimalInterface{
     public String getHealth(){
         return health;
     }
+
+    public static List<EndangeredAnimal> all() {
+        String sql = "SELECT * FROM animals WHERE endangered = 'true'";
+        try(Connection con = DB.sql2o.open()) {
+          return con.createQuery(sql).throwOnMappingFailure(false)
+            .executeAndFetch(EndangeredAnimal.class);
+        }
+      }
+
+ 
+      public static EndangeredAnimal find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+          String sql = "SELECT * FROM animals where id=:id";
+          EndangeredAnimal animal = con.createQuery(sql)
+            .addParameter("id", id)
+            .throwOnMappingFailure(false)
+            .executeAndFetchFirst(EndangeredAnimal.class);
+          if(animal == null){
+            throw new IndexOutOfBoundsException("Sorry, this animal cannot be found.");
+          }
+          return animal;
+        }
+      }
+
+      @Override
+      public void save() {
+        super.save(); //calls the parent constructor with no arguments
+        try(Connection con = DB.sql2o.open()) {
+          String sql = "UPDATE animals SET health=:health, age=:age WHERE id=:id";
+          con.createQuery(sql).addParameter("health", this.health)
+            .addParameter("age", this.age)
+            .addParameter("id", this.id)
+            .executeUpdate();
+        }
+      }
     
    
 }
