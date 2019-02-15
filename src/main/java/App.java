0d75fb193ff8b0ae96a0e44b76cd7be549e67e5d
liciolentimo <lentimo@gmail.com>
2019-02-15 11:49:09 +0300

    [Finishes #164000302] Added route for homepage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,5 +10,11 @@ public class App {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
     }
 }
