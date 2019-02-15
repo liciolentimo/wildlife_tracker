a05ed35fbb11585f5b6e50062716bd8b6662744a
liciolentimo <lentimo@gmail.com>
2019-02-15 11:47:04 +0300

    [Finishes #164000798] Added not endangered class

---
+++ src/main/java/NotEndangered.java
@@ -0,0 +1,7 @@
+public class NotEndangered extends Animal{
+    public NotEndangered(String name, String species){
+        this.name = name;
+        this.species = species;
+        endangered = false;
+    }
+}
