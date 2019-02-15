fe68ddb7964d851cf582895973d3c2b4d5979677
liciolentimo <lentimo@gmail.com>
2019-02-15 11:32:44 +0300

    [Finishes #163998807] Adds abstract class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,28 +1,57 @@
-public class Animal{
-    private String name;
-    private String age;
-    private String health;
-    private int id;
+import java.util.List;
+import org.sql2o.*;
 
-    public Animal(String name, String age, String health){
-        this.name = name;
-        this.age = age;
-        this.health = health;
-    }
+public abstract class Animal{
+    public String name;
+    public String age;
+    public int id;
+    public boolean endangered;
+    public String species;
 
     public String getName(){
         return name;
     }
 
-    public String getAge(){
-        return age;
-    }
-
-    public String getHealth(){
-        return health;
-    }
-    
     public int getId(){
         return id;
     }
+
+    public boolean isEndangered(){
+        return endangered;
+    }
+
+    public String getSpecies(){
+        return species;
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal){
+      if (!(otherAnimal instanceof Animal)) {
+        return false;
+      } else {
+        Animal newAnimal = (Animal) otherAnimal;
+        return this.id == newAnimal.id && this.name.equals(newAnimal.name) &&
+          this.species.equals(newAnimal.species);
+      }
+    }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+          String sql = "INSERT INTO animals (name, endangered, species) VALUES (:name, :endangered, :species)";
+          this.id = (int) con.createQuery(sql, true)
+            .addParameter("name", this.name)
+            .addParameter("endangered", this.endangered)
+            .addParameter("species", this.species)
+            .executeUpdate()
+            .getKey();
+        }
+      }
+
+      public List<Sighting> getSightings(){
+        try(Connection con = DB.sql2o.open()){
+          String sql = "SELECT sightings.* FROM sightings JOIN animals_sightings ON (sightings.id = animals_sightings.sighting_id) WHERE animals_sightings.animal_id = :id";
+          return con.createQuery(sql).addParameter("id", this.id).executeAndFetch(Sighting.class);
+        }
+      }
+  
 }
